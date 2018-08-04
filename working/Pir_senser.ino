93ba57291dc0980ee4cb9a8c8adaece25ca01429
kanokwan0049 <42020206+kanokwan0049@users.noreply.github.com>
2018-08-04 13:11:38 +0700

    Add files via upload

---
+++ working/Pir_senser.ino
@@ -0,0 +1,19 @@
+int pirPin = 2; //digital 2
+int ledPin = 13;
+
+void setup(){
+ Serial.begin(9600); 
+ pinMode(pirPin, INPUT);
+ pinMode(ledPin, OUTPUT);
+}
+
+void loop(){
+  int pirVal = digitalRead(pirPin);
+  Serial.println(pirVal); 
+  if(pirVal == LOW){ //was motion detected
+    Serial.println("Motion Detected");
+    digitalWrite(ledPin, HIGH);
+    delay(2000); 
+  }
+  digitalWrite(ledPin, LOW);
+}
